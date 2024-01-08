4f97ef73382737c557c633ca71584aac82ade946
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-08 12:51:57 +0100

    starting jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,10 @@ const usersDB = {
     setUsers: function (data) { this.users = data }
 }
 const bcrypt = require('bcrypt');
+const jwt=require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises=require('fs').promises;
+const path=require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
