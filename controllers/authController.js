a443243d5a3cdd2a5a4acdc98ab93ec1333fa72b
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-10 17:14:00 +0100

    worked on logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+
+
 const usersDB = {
     users: require('../model/users.json'),
     setUsers: function (data) { this.users = data }
@@ -9,6 +11,7 @@ const fsPromises=require('fs').promises;
 const path=require('path');
 
 const handleLogin = async (req, res) => {
+
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
     const foundUser = usersDB.users.find(person => person.username === user);
---
+++ controllers/logoutController.js
@@ -0,0 +1,41 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
+
+const  fsPromises=require('fs').promises;
+const path=require('path');
+
+
+
+
+const jwt=require('jsonwebtoken');
+require('dotenv').config();
+
+const handleLogout =   async(req, res) => {
+    //on client,also delete the accesstoken
+    
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204); //no content
+    const refreshToken=cookies.jwt;
+    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    if (!foundUser) {
+        res.clearCookie("jwt",{httpOnly:true});
+        return res.sendStatus(204); //forbidden
+    }
+
+    //Delete refreshToken in db
+    const otherUsers=usersDB.users.find(person => person.refreshToken!== foundUser.refreshToken);
+    const currentUser={...otherUsers,refreshToken:''}
+    usersDB.setUsers([...otherUsers,currentUser]);
+    await fsPromises.writeFile(
+        path.join(__dirname, '..' ,'model','user.json'),
+        JSON .stringify(usersDB.user)
+    );
+    
+    res.clearCookie( 'jwt', {httpOnly: true}); //secure true only server on true
+
+    res.sendStatusCode(204);
+}
+
+module.exports = { handleLogout }
